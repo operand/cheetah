3c0e2b6fa3bb60be396c49ef9e6b885323c8eedb
Dan Rodriguez <theoperand@gmail.com>
2011-05-05 14:56:43 -0400

    added http blocker to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
 require 'cheetah'
+require 'net/http'
 
 CM_SUB_ID           = "2087039029"           # the test mailing list id
 CM_WHITELIST_FILTER = /@buywithme\.com$/     # if set, emails will only be sent to addresses which match this pattern
 CM_ENABLE_TRACKING  = false                  # determines whether cheetahmail will track the sending of emails for statistical purposes. this should be disabled outside of production 
 CM_MESSENGER        = Cheetah::NullMessenger # the class used for sending api requests to cheetahmail.
+
+
+Spec::Runner.configure do |config|                                                                                                                        
+  config.before(:each) do
+    Net::HTTP.stub(:new).and_return(mock(:http).as_null_object) # to block any http requests
+  end
+end
+
