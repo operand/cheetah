74dec86e167e28cdc58ad0aa46f06b5865a2e06e
Dan Rodriguez <theoperand@gmail.com>
2011-05-05 16:57:50 -0400

    created NullMessenger spec

--- spec/null_messenger_spec.rb
+++ spec/null_messenger_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Cheetah::NullMessenger do
   context "#send" do
-    it "should do nothing"
+    it "should do nothing" do
+      @http = mock(:http)
+      Net::HTTP.stub(:new).and_return(@http)
+      Cheetah::NullMessenger.instance.send(Message.new("/",{}))
+    end
   end
 end
