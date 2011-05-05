b76586f6ff08e4c39ff5864d3189d83493579796
Dan Rodriguez <theoperand@gmail.com>
2011-05-05 11:35:06 -0400

    made second spec pass

--- spec/cheetah_spec.rb
+++ spec/cheetah_spec.rb
@@ -12,14 +12,23 @@ describe Cheetah do
   end
 
   context '.send_email' do
+    before do
+      @message = Message.new('/ebm/ebmtrigger1', @params)
+    end
+
     it "should send a Message object to the messenger instance" do
-      message = Message.new('/ebm/ebmtrigger1', @params)
-      Message.should_receive(:new).with('/ebm/ebmtrigger1', @params).and_return(message)
-      CM_MESSENGER.instance.should_receive(:send).with(message)
+      Message.should_receive(:new).with('/ebm/ebmtrigger1', @params).and_return(@message)
+      CM_MESSENGER.instance.should_receive(:send).with(@message)
       Cheetah.send_email(@eid, @email)
     end
 
-    it "should suppress emails that do not match the whitelist"
+    it "should suppress emails that do not match the whitelist" do
+      @email = 'foo@bar.com'
+      @params['email'] = @email
+      Message.should_receive(:new).with('/ebm/ebmtrigger1', @params).and_return(@message)
+      CM_MESSENGER.instance.should_not_receive(:send)
+      Cheetah.send_email(@eid, @email)
+    end
   end
 
   context '.mailing_list_update' do
