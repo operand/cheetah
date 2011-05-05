0f9e8d02912f1a132d3c1da545f5576da43d855c
Dan Rodriguez <theoperand@gmail.com>
2011-05-05 16:41:01 -0400

    made synchronous messenger spec work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 require 'cheetah'
 require 'net/http'
 
-CM_SUB_ID           = "2087039029"           # the test mailing list id
+CM_HOST             = "foo.com"
+CM_USERNAME         = "foo_user"
+CM_PASSWORD         = "foo"
+CM_AID              = "123"                  # our 'affiliate id' for 'BuyWithMe'. we currently only use one 'affiliate' so this is pretty static
+CM_SUB_ID           = "456"                  # the test mailing list id
 CM_WHITELIST_FILTER = /@buywithme\.com$/     # if set, emails will only be sent to addresses which match this pattern
 CM_ENABLE_TRACKING  = false                  # determines whether cheetahmail will track the sending of emails for statistical purposes. this should be disabled outside of production 
 CM_MESSENGER        = Cheetah::NullMessenger # the class used for sending api requests to cheetahmail.
--- spec/synchronous_messenger_spec.rb
+++ spec/synchronous_messenger_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Cheetah::SynchronousMessenger do
-  it "should do something"
+
+  context "#send" do
+    before do
+      @messenger = Cheetah::SynchronousMessenger.instance
+      @message   = Message.new("/",{})
+      @http      = mock(:http).as_null_object
+      @resp      = mock(:resp)
+      Net::HTTP.stub(:new).and_return(@http)
+    end
+
+    it "should raise CheetahAuthorizationException when there's an authorization problem" do
+      pending
+      lambda { @messenger.send(@message) }.should raise_error(CheetahAuthorizationException)
+    end
+
+    it "should raise CheetahPermanentException when there's a permanent error on Cheetah's end" do
+      pending
+      lambda { @messenger.send(@message) }.should raise_error(CheetahPermanentException)
+    end
+
+    it "should raise CheetahTemporaryException when there's a temporary error on Cheetah's end" do
+      @resp.stub(:code).and_return('500')
+      @http.stub(:post).and_return(@resp)
+      lambda { @messenger.send(@message) }.should raise_error(CheetahTemporaryException)
+    end
+
+  end
+
 end
