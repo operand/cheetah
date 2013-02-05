0761f94e05d8e993e5aa955f836fc873fa75c8cd
Wilfred Boayue <wil@moduscreate.com>
2013-02-05 18:37:56 -0500

    successful send with resque
    Signed-off-by: Wilfred Boayue <wil@moduscreate.com>

--- lib/cheetah/messenger/resque_transactional_messenger.rb
+++ lib/cheetah/messenger/resque_transactional_messenger.rb
@@ -1,14 +1,22 @@
+require 'cheetah/transactional_message'
+require 'cheetah/messenger/transactional_messenger'
+
 module Cheetah
   
   class ResqueTransactionalMessenger
 
-    def send(message)
+    @queue = :cheetah
+
+    def initialize(options = {})
+    end
+
+    def send_message(message)
       Resque.enqueue(self.class, message.to_params)
     end
 
-    def self.perform(message)
+    def self.perform(params)
       messenger = TransactionalMessenger.new
-      messenger.send(message)
+      messenger.send_message(TransactionalMessage.new(params))
     end
 
   end
--- lib/cheetah/transactional_message.rb
+++ lib/cheetah/transactional_message.rb
@@ -1,18 +1,10 @@
 class TransactionalMessage
 
-  attr_reader :aid, :email, :params, :attachments
+  attr_reader :params, :attachments
 
   def initialize(params, attachments = {})
     @params = params
     @attachments = attachments
-  end
-
-  def aid
-    @params[:AID]
-  end
-
-  def email
-    @params[:email]
   end
 
   def to_params
