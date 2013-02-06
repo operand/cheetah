5cadefbdea8cb520d4206a29458fdd61f010e088
Wilfred Boayue <wil@moduscreate.com>
2013-02-05 21:51:28 -0500

    update spec for transactional messenger

--- spec/transactional_messenger_spec.rb
+++ spec/transactional_messenger_spec.rb
@@ -4,17 +4,37 @@ describe Cheetah::TransactionalMessenger do
   
   before do
     @messenger = Cheetah::TransactionalMessenger.new
-    stub_http
   end
 
-  context "#send" do
-    before do
-      @message   = TransactionalMessage.new({})
+  context "#send_message" do
+    it "should post parameters cheetahmail endpoints" do
+      params = {foo: 'bar'}
+      expected_params = {body: {:ACTION => "SYSTEM"}}
+      expected_params[:body].merge! params
 
-      @resp      = mock(:resp).as_null_object
-      @http      = mock(:http).as_null_object
-      @http.stub(:post).and_return(@resp)
-      Net::HTTP.stub(:new).and_return(@http)
+      Cheetah::TransactionalMessenger.default_options[:base_uri].should eql('http://sysmail.fagms.net')
+
+      response = {"systemmail_result" => { "emstatuscodes" => "+10"}}
+
+      @message = Message.new(nil, params)
+
+      Cheetah::TransactionalMessenger.should_receive(:post).with("/c/sm", expected_params).and_return(response)
+
+      @messenger.send_message(@message)
+    end
+
+    it "should raise an exception for status code indicating errors" do
+      params = {foo: 'bar'}
+      expected_params = {body: {:ACTION => "SYSTEM"}}
+      expected_params[:body].merge! params
+
+      response = {"systemmail_result" => { "emstatuscodes" => "-40"}}
+
+      @message = Message.new(nil, params)
+
+      Cheetah::TransactionalMessenger.should_receive(:post).with("/c/sm", expected_params).and_return(response)
+
+      lambda { @messenger.send_message(@message) }.should raise_error(CheetahException)
     end
 
   end
